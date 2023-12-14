0f4f9393c5982f6d5dfff751915b3d7242421e22
Victor Torres <vhugosoria2@gmail.com>
2023-12-14 17:21:16 -0300

    Configurand o adapter

--- app/src/androidTest/java/br/com/alura/orgsalura/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/alura/orgsalura/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package br.com.alura.orgsalura
+package br.com.alura.orgsalura.ui.activity
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
